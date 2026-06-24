ac3ae3c1e0fa46cf3cc18b2cc565f95a99dd55ca
A11hmaad <naba2110@gmail.com>
2026-06-24 15:44:18 +0600

    Create js/firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwktvktXaaRxFPX500kOBcvuUtOjRjz6M",
+    authDomain: "wakanda-shit.firebaseapp.com",
+    databaseURL: "https://wakanda-shit-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "wakanda-shit",
+    storageBucket: "wakanda-shit.firebasestorage.app",
+    messagingSenderId: "491323769717",
+    appId: "1:491323769717:web:825b6044e43e672404493c"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
